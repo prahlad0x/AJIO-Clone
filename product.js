520e13babef746943ca9874a4217a8f89410d585
Gaurav Asode <gauravasode5@gmail.com>
2023-02-24 23:01:31 +0530

    Day 4 work done

--- product.js
+++ product.js
@@ -2272,9 +2272,11 @@ SareesEl.addEventListener("change" , (e)=>{
         })
         Display(filterData);
         document.querySelector(".product-desc").textContent = `${SareesEl.value}`;
+        document.querySelector(".about").textContent= `Traditional, trendy, stylish and graceful, there’s no denying that a woman’s beauty is accentuated by a saree. When you are shopping online for designer sarees for festive occasions or regular sarees for casual ethnic wear, you won’t have to look further than AJIO. From gorgeous silk sarees to crisp cottons, you’ll be amazed with the selection from brands such as Indie`
     }else{
         Display(API);
         document.querySelector(".product-desc").textContent = " All Product";
+        document.querySelector(".about").textContent= ""
     }
 });
 
@@ -2287,9 +2289,12 @@ ShoesEl.addEventListener("change" , (e)=>{
         })
         Display(filterData);
         document.querySelector(".product-desc").textContent = `${ShoesEl.value}`;
+        document.querySelector(".about").textContent= `Casual Shoes - Header
+        Get your casual style on point with AJIO’s collection of men’s casual shoes . From smooth slip-ons to snazzy loafers, the collection comprises of the trendiest in every style. Go ahead and step into the world of fashion, with the coolest of shoes`
     }else{
         Display(API);
         document.querySelector(".product-desc").textContent = " All Product";
+        document.querySelector(".about").textContent= ""
     }
 });
 
