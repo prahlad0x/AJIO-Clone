df812b323d70886324e0ec52dd895c502aed6fff
unknown <anuragkushwah134@gmail.com>
2023-02-26 16:17:11 +0530

    demo

--- detail.js
+++ detail.js
@@ -1,23 +1,9 @@
 
-let uesrsName = localStorage.getItem('userName') || '';
-let singupPage = document.getElementById('loginregister')
-
-  singupPage.addEventListener('click',()=>{
-    if(uesrsName == ''){
-      location.href='logReg.html'
-    }
-    else{
-      location.href ='cart.html'
-    }
-  })
-
-  
 let AllProducts = JSON.parse(localStorage.getItem('allProducts')) ||[]
 product=localStorage.getItem("ProductId")||''
 // console.log(product)
 let bagproduct=JSON.parse(localStorage.getItem("bagproduct"))||[]
 let wishproduct=JSON.parse(localStorage.getItem("wishproduct"))||[]
-
 let btn1=document.getElementById("bag")
 let btn2=document.getElementById("wishlist")
 let image=document.getElementById("imj")
@@ -32,62 +18,32 @@ function showdata(data){
           color.innerText=data.color
           title.innerText=data.title
 
-          flag = false;
-    for(let i = 0; i<bagproduct.length; i++){
-      if(data.id == bagproduct[i]){
-        flag = true;
-      }
-    }
-    if(!flag){
-      btn1.textContent = "Add To Bag";
-    }
-    else{
-      
-      btn1.textContent = "Remove from Bag";
-    }
-
           btn1.addEventListener('click',(e)=>{
             if(btn1.innerText == "Add To Bag"){
                 bagproduct.push(data.id);
                 localStorage.setItem("bagproduct",JSON.stringify(bagproduct));
-                btn1.innerText = "Remove from Bag";
+                btn1.textContent = "Remove from Bag";
                 alert("Product Added to the Bag")
-              }else if(btn1.innerText == "Remove from Bag"){
+              }else if(Button2.innerText == "Remove from Bag"){
                 bagproduct = bagproduct.filter((el) => el != data.id);
                 localStorage.setItem("bagproduct",JSON.stringify(bagproduct));
                 btn1.innerText = "Add To Bag";
                 alert("Product removed from bag");
               } 
       })
-
-
-      flag2 = false;
-    for(let i = 0; i<wishproduct.length; i++){
-      if(data.id == wishproduct[i]){
-        flag2 = true;
-      }
-    }
-    if(!flag2){
-      btn2.textContent = "Save To Wishlist";
-    }
-    else{
-      
-      btn2.textContent = "Remove from Wishlist";
-    }
       btn2.addEventListener('click',(e)=>{
         if(btn2.innerText == "Save To Wishlist"){
             wishproduct.push(data.id);
             localStorage.setItem("wishproduct",JSON.stringify(wishproduct));
             btn2.textContent = "Remove from Wishlist";
-            alert("Product Added to the Wishlist")
+            alert("Product Added to the Bag")
           }else if(btn2.innerText == "Remove from Wishlist"){
             wishproduct = wishproduct.filter((el) => el != data.id);
             localStorage.setItem("wishproduct",JSON.stringify(wishproduct));
-            btn2.innerText = "Save To Wishlist";
+            btn2.innerText = "Add To Bag";
             alert("Product removed from wishlist");
           } 
     })
-
 
     
      }
@@ -95,9 +51,7 @@ function showdata(data){
         for(i=0;i<AllProducts.length;i++){
             if(AllProducts[i].id==id){
                 showdata(AllProducts[i])
-
-                // console.log(AllProducts[i])
-
+                console.log(AllProducts[i])
                 break;
             }
         }
