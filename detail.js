91103e27d43dec0ab4f688a6fcb52d4fc8824718
unknown <anuragkushwah134@gmail.com>
2023-02-26 16:24:30 +0530

    completed

--- detail.js
+++ detail.js
@@ -18,29 +18,57 @@ function showdata(data){
           color.innerText=data.color
           title.innerText=data.title
 
+          flag = false;
+    for(let i = 0; i<bagproduct.length; i++){
+      if(data.id == bagproduct[i]){
+        flag = true;
+      }
+    }
+    if(!flag){
+      btn1.textContent = "Add To Bag";
+    }
+    else{
+      
+      btn1.textContent = "Remove from Bag";
+    }
+
           btn1.addEventListener('click',(e)=>{
             if(btn1.innerText == "Add To Bag"){
                 bagproduct.push(data.id);
                 localStorage.setItem("bagproduct",JSON.stringify(bagproduct));
                 btn1.textContent = "Remove from Bag";
                 alert("Product Added to the Bag")
-              }else if(Button2.innerText == "Remove from Bag"){
+              }else if(btn1.innerText == "Remove from Bag"){
                 bagproduct = bagproduct.filter((el) => el != data.id);
                 localStorage.setItem("bagproduct",JSON.stringify(bagproduct));
                 btn1.innerText = "Add To Bag";
                 alert("Product removed from bag");
               } 
       })
+      
+      flag2 = false;
+      for(let i = 0; i<wishproduct.length; i++){
+        if(data.id == wishproduct[i]){
+          flag2 = true;
+        }
+      }
+      if(!flag2){
+        btn2.textContent = "Save To Wishlist";
+      }
+      else{
+        
+        btn2.textContent = "Remove from Wishlist";
+      }
       btn2.addEventListener('click',(e)=>{
         if(btn2.innerText == "Save To Wishlist"){
             wishproduct.push(data.id);
             localStorage.setItem("wishproduct",JSON.stringify(wishproduct));
             btn2.textContent = "Remove from Wishlist";
-            alert("Product Added to the Bag")
+            alert("Product Added to the Wishlist")
           }else if(btn2.innerText == "Remove from Wishlist"){
             wishproduct = wishproduct.filter((el) => el != data.id);
             localStorage.setItem("wishproduct",JSON.stringify(wishproduct));
-            btn2.innerText = "Add To Bag";
+            btn2.innerText = "Save To Wishlist";
             alert("Product removed from wishlist");
           } 
     })
